f7ea2c7db7c2ffc04ae235a121df99e52e8be7f5
edith1525 <edith.boros.eb@gmail.com>
2025-11-21 20:55:29 +0200

    aproape am terminat

--- src/test/java/MyFirstSeleniumTest.java
+++ src/test/java/MyFirstSeleniumTest.java
@@ -10,7 +10,7 @@ import java.awt.*;
 import java.time.Duration;
 
 public class MyFirstSeleniumTest {
-    static void main(String[] args) throws InterruptedException {
+    static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
@@ -25,8 +25,18 @@ public class MyFirstSeleniumTest {
         WebElement pimMenu = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href='/web/index.php/pim/viewPimModule']")));
         pimMenu.click();
 
-        WebElement newUser = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("(//input[@placeholder='Type for hints...'])[1]")));
-        newUser.sendKeys("a b c");
+        WebElement newUser = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@placeholder='Type for hints...'])[1]")));
+        newUser.sendKeys("Ravi M B");
+
+        WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
+        searchButton.click();
+
+        WebElement userPage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Ravi M']")));
+        userPage.click();
+
+        WebElement contactDetails = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Contact Details']")));
+        contactDetails.click();
+
 
 
 
